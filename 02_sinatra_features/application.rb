e8fa125a3f6722617838787f7e8ab4032ab6a2a7
Karel Minarik <karmi@karmi.cz>
2009-02-05 14:25:17 +0100

    [02] Demonstrate configuration and environments

--- 02_sinatra_features/application.rb
+++ 02_sinatra_features/application.rb
@@ -1,11 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 
-before do
-  puts request.env['HTTP_USER_AGENT']
-  halt 417, 'Expectation Failed' unless request.env['HTTP_USER_AGENT'] =~ /Firefox/
+# $ ruby application.rb
+configure do
+  PASSWORD = 'password'
 end
 
-get '/' do
-  'Oh &hearts; welcome, Firefox user!'
+# $ ruby application.rb -e production -p 5678
+configure :production do
+  PASSWORD = '5f4dcc3b5aa765d61d8327deb882cf99'
+end
+
+get '/:password' do
+  params[:password] == PASSWORD ? '&iexcl;Welcome!' : 'What?'
 end
