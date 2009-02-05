4cdcb8e6c709294a2b0f48db2b7ce0afde85b264
Karel Minarik <karmi@karmi.cz>
2009-02-05 14:47:37 +0100

    [02] Add simple error handling

--- 02_sinatra_features/application.rb
+++ 02_sinatra_features/application.rb
@@ -5,11 +5,17 @@ before do
   @views = Dir['views/*.*'].entries.collect { |f| { :name => File.basename(f), :size => File.size(f) } }
 end
 
+error do
+  "We're so sorry... #{request.env['sinatra.error'].message}"
+end
+
 get '/' do
   erb :index
 end
 
 # GET http://localhost:4567/views/index.erb
+# GET http://localhost:4567/views/item.XXXXX => Error 500
+# (Errors are handled differently in :development and :production)
 get %r{/views/(.+\..{1,4})} do
   filename = params[:captures]
   @file = { :name => filename, :contents => File.read("views/#{filename}") }
