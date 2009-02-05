e9f2498dc55adcfeabc06e43ef244fb031f734ea
Karel Minarik <karmi@karmi.cz>
2009-02-05 11:17:16 +0100

    More demonstration of middleware use -- use Rack::ShowExceptions (and deliberately break the code)

--- 01_rack/minirack.rb
+++ 01_rack/minirack.rb
@@ -5,11 +5,12 @@ require 'rack'
 app = Proc.new do |env|
         [ 200,
         {'Content-Type' => 'text/plain'},
-        "Received params: #{Rack::Request.new(env).params.inspect}" ]
+        "Received params: #{XXX___this_is_the_fail___XXXRack::Request.new(env).params.inspect}" ]
 end
 
 stack = Rack::Builder.app do
  use Rack::CommonLogger
+ use Rack::ShowExceptions
  run app
 end
 
