85e092a13ec0ec27931627eb00d88f30f3879fc2
Karel Minarik <karmi@karmi.cz>
2009-02-05 13:32:58 +0100

    [02] Add route examples

--- 02_sinatra_features/application.rb
+++ 02_sinatra_features/application.rb
@@ -4,3 +4,21 @@ require 'sinatra'
 get '/' do
   "Hello, world!"
 end
+
+get '/special' do
+  "Hello, special!"
+end
+
+get '/:name' do
+  "Hello, #{params[:name]}!"
+end
+
+# GET http://localhost:4567/download/file.txt
+get '/download/*.*' do
+  "Got #{params[:splat].inspect}"  # Broken in < 0.9.0.4 (eats first letter)
+end
+
+# GET http://localhost:4567/hello/123-start
+get %r{/hello/(\d{0,4})\-([\w]+)} do
+  "Hello, #{params[:captures].inspect}!"
+end
