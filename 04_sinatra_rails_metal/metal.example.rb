8f002a57f688d6814b1da00041644ac033df46d8
Karel Minarik <karmi@karmi.cz>
2009-02-06 01:45:39 +0100

    [04] Add infrastructure for trying Sinatra in Rails Metal \,,/ (use Rake tasks like rake rails:up)

---
+++ 04_sinatra_rails_metal/metal.example.rb
@@ -0,0 +1,16 @@
+# Allow the metal piece to run in isolation
+require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
+
+require 'sinatra/base'
+
+class Api < Sinatra::Base
+
+  enable :logging
+
+  get '/api/products/:id/in_stock.json' do
+   response['Content-Type'] = 'application/json; charset=utf-8'
+   @product = Product.find params[:id]
+   @product.to_json(:only => ['id', 'name', 'in_stock'])
+  end
+
+end
