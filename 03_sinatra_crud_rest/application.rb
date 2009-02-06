08ddd1143e3529fc90700f1eb46307f34cff0f4a
Karel Minarik <karmi@karmi.cz>
2009-02-06 09:42:48 +0100

    [03] Basic CRUD application -- step 2 : Add routes and model validations

--- 03_sinatra_crud_rest/application.rb
+++ 03_sinatra_crud_rest/application.rb
@@ -8,3 +8,47 @@ configure do
   ActiveRecord::Base.establish_connection( :adapter => 'sqlite3', :database => 'notes.db')
   load 'lib/schema.rb' unless Note.table_exists?
 end
+
+# List: GET /
+get '/' do
+  @notes = Note.all(:order => 'created_at DESC')
+  haml :index
+end
+
+# Detail: GET /1
+get '/:id/?' do
+  @note = Note.find params[:id]
+  haml :show
+end
+
+# Edit: GET /1/edit
+get '/:id/edit' do
+  @note = Note.find params[:id]
+  haml :edit
+end
+
+# Create: POST /
+post '/' do
+  @note = Note.create( params[:note] )
+  if @note.valid?
+    session[:success] = 'Created new note'
+    redirect '/' and return
+  else
+    @notes = Note.all(:order => 'created_at DESC')
+    haml :index
+  end
+end
+
+# Update: PUT /1
+put '/:id' do
+  @note = Note.find params[:id]
+  @note.update_attributes params[:note]
+  redirect '/'
+end
+
+# Destroy: DELETE /1
+delete '/:id' do
+  @note = Note.destroy params[:id]
+  session[:success] = 'Note was successfully deleted'
+  redirect '/'
+end
--- 03_sinatra_crud_rest/lib/note.rb
+++ 03_sinatra_crud_rest/lib/note.rb
@@ -1,2 +1,9 @@
 class Note < ActiveRecord::Base
+
+  validates_presence_of :body
+
+  def permalink
+    "/#{id}-#{body.to_s[0...20].downcase.gsub(/[^\w]+/, '-')}"
+  end
+
 end
